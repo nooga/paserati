dac72520a89f1f52e66e12c8f23a48466bdc5a6f
Marcin Gasperowicz <xnooga@gmail.com>
2026-09-08 16:20:16 +0100

    chore(builtins): remove dead SetupTypedArrayToStringTag helper

--- pkg/builtins/typedarray_common.go
+++ pkg/builtins/typedarray_common.go
@@ -590,12 +590,3 @@ func SetupTypedArrayPrototypeProperties(proto *vm.PlainObject, vmInstance *vm.VM
 	w, e, c := false, false, false
 	proto.DefineOwnProperty("BYTES_PER_ELEMENT", vm.Number(float64(bytesPerElement)), &w, &e, &c)
 }
-
-// SetupTypedArrayToStringTag adds Symbol.toStringTag getter to a TypedArray prototype.
-// Per ECMAScript spec, %TypedArray%.prototype[@@toStringTag] is "TypedArray" and
-// each specific TypedArray prototype has its own [@@toStringTag] (e.g., "Uint8Array").
-// The property is: { writable: false, enumerable: false, configurable: true }
-func SetupTypedArrayToStringTag(proto *vm.PlainObject, typedArrayName string) {
-	e, c := false, true // not enumerable, configurable
-	proto.DefineOwnPropertyByKey(vm.NewSymbolKey(SymbolToStringTag), vm.NewString(typedArrayName), nil, &e, &c)
-}
